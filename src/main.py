02d58c769487e71f2f26c228991c3b16eedee0d9
Kacper Kafara <kacperkafara@gmail.com>
2023-12-05 20:56:15 +0100

    Computing subgraph isomorphisms & drawing the graph (#2)
    * Some renaming & function prototypes
    
    * Computing isomorphisms & drawing the graph

--- src/main.py
+++ src/main.py
@@ -1,29 +1,46 @@
 import networkx as nx
 import graphviz
-from model.graph import Graph
-from model.node import Node
-from model.point import Point
+import itertools as it
+import matplotlib.pyplot as plt
+from model.graph import Graph, Node, NodeAttrs, EdgeAttrs, EdgeType
 
 def main():
     graph = Graph()
-    node_1 = Node(0, 'root', Point(0, 0))
-    node_2 = Node(1, '1', Point(1, 0))
-    node_3 = Node(2, '2', Point(1, 1))
 
-    graph.add_node(node_1)
-    graph.add_node(node_2)
-    graph.add_node(node_3)
+    node_attrs = (
+        NodeAttrs('first', 0, 0),
+        NodeAttrs('second', 1, 1),
+        NodeAttrs('third', 2, 4)
+    )
 
-    graph.add_edge(node_1, node_2, {'foo': 'bar'})
-    graph.add_edge(node_1, node_3, {'42': '84'})
+    bundles = list(zip(range(len(node_attrs)), node_attrs))
+    graph.add_node_collection(bundles)
 
-    print(graph.backing_graph().nodes)
-    print(graph.backing_graph().edges)
-    print(list(graph.backing_graph().edges)[0])
+    for (node_1, _), (node_2, _) in it.pairwise(bundles):
+        graph.add_edge(node_1, node_2, attrs={'foo': 'bar'})
 
-    graph_for_vis = nx.nx_agraph.to_agraph(graph.backing_graph())
-    graph_for_vis.draw("graph.png", prog='dot')
+    print(graph.nx_graph.nodes)
+    print(graph.nx_graph.edges)
 
+    subgraph = Graph()
+    subgraph.add_node_collection(bundles[:2])
+    for (node_1, _), (node_2, _) in it.pairwise(bundles[:2]):
+        subgraph.add_edge(node_1, node_2, attrs=None)
+
+    gm = nx.isomorphism.GraphMatcher(graph.nx_graph, subgraph.nx_graph)
+
+    print('Mapping')
+    for mapping in gm.subgraph_isomorphisms_iter():
+        print(mapping)
+
+    fig, plots = plt.subplots(nrows=1, ncols=2)
+    graph.display(ax=plots[0])
+    subgraph.display(ax=plots[1])
+
+    fig.savefig('graphs.png')
+    fig.tight_layout()
+    plt.show()
 
 if __name__ == '__main__':
+    plt.rcParams['figure.figsize'] = (16, 9)
     main()
