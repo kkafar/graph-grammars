1f49b7b504d7a7f0ded06a3d415f6cf6fd9f057d
macsko <maskskoczen@gmail.com>
2023-12-09 12:33:45 +0100

    Add productions 5 and 6

--- src/production/__init__.py
+++ src/production/__init__.py
@@ -1,11 +1,15 @@
 from .model import Production
 from .production_1 import P1
 from .production_2 import P2
+from .production_5 import P5
+from .production_6 import P6
 
 
 __all__ = (
     Production,
     P1,
-    P2
+    P2,
+    P5,
+    P6
 )
 
