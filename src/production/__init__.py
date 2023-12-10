73b8ed44ea23536a0f6d8a3138b4a1e0e4926786
MichalZelasko <zelaskomj@gmail.com>
2023-12-10 17:58:24 +0100

    Update __init__.py

--- src/production/__init__.py
+++ src/production/__init__.py
@@ -1,4 +1,19 @@
+from .model import Production
+from .production_1 import P1
+from .production_2 import P2
+from .production_5 import P5
+from .production_6 import P6
 from .production_11 import P11
 from .production_12 import P12
+
+
+__all__ = (
+    Production,
+    P1,
+    P2,
+    P5,
+    P6,
     P11,
     P12
+)
+
