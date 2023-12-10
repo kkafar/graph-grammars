ea1163c1a9bb77d34e5466b9bba7a1516b11f8e2
MichalZelasko <zelaskomj@gmail.com>
2023-12-10 17:58:45 +0100

    Update main.py
    Testing productions P11 and P12

--- src/main.py
+++ src/main.py
@@ -175,10 +175,58 @@ def monomorphisms():
     print(f"Isomorphic count: {len(isomorphic_mappings)}, Monomorhpic count: {len(monomorphic_mappings)}")
 
 
+def test_production11():
+    graph = Graph()
+
+    node_0 = Node(NodeAttrs('v', 0, 0, False))
+    node_1 = Node(NodeAttrs('v', 1, 0, False))
+    node_2 = Node(NodeAttrs('v', 1, 1, False))
+    node_3 = Node(NodeAttrs('v', 0, 1, False))
+    node_4 = Node(NodeAttrs('v', 1.83, 0.5, False))
+    node_5 = Node(NodeAttrs('v', 0.5, 0, True))
+    node_6 = Node(NodeAttrs('v', 0, 0.5, True))
+    nodes = [node_0, node_5, node_1, node_4, node_2, node_3, node_6]
+    corner_nodes = (node_0, node_1, node_2, node_3, node_4)
+
+    graph.add_node_collection(nodes)
+
+    graph.add_p_hyperedge(corner_nodes, EdgeAttrs('p', True))
+
+    for node_a, node_b in it.pairwise(nodes + [node_0]):
+        graph.add_edge(Edge(node_a.handle, node_b.handle, EdgeAttrs(kind='e', flag=False)))
+
+    P11()(graph)
 
 
+def test_production12():
+    graph = Graph()
+
+    node_0 = Node(NodeAttrs('v', 0, 0, False))
+    node_1 = Node(NodeAttrs('v', 1, 0, False))
+    node_2 = Node(NodeAttrs('v', 1, 1, False))
+    node_3 = Node(NodeAttrs('v', 0, 1, False))
+    node_4 = Node(NodeAttrs('v', 1.83, 0.5, False))
+    node_5 = Node(NodeAttrs('v', 0.5, 0, True))
+    node_6 = Node(NodeAttrs('v', 0.5, 1, True))
+    nodes = [node_0, node_5, node_1, node_4, node_2, node_6, node_3]
+    corner_nodes = (node_0, node_1, node_2, node_3, node_4)
+
+    graph.add_node_collection(nodes)
+
+    graph.add_p_hyperedge(corner_nodes, EdgeAttrs('p', True))
+
+    for node_a, node_b in it.pairwise(nodes + [node_0]):
+        graph.add_edge(Edge(node_a.handle, node_b.handle, EdgeAttrs(kind='e', flag=False)))
+
+    P12()(graph)
 
 
 if __name__ == '__main__':
     plt.rcParams['figure.figsize'] = (16, 9)
+    # test_production()
+    # test_production2()
+    # test_production5()
+    # test_production6()
+    # test_production11()
+    test_production12()
     # main()
