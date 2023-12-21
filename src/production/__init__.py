c103aff60f28fa3d94c2eb307ebe372f3ef9b392
a-float <mati852456@gmail.com>
2023-12-22 00:07:24 +0100

    Add production 4

--- src/production/__init__.py
+++ src/production/__init__.py
@@ -2,6 +2,7 @@ from .model import Production
 from .production_1 import P1
 from .production_2 import P2
 from .production_3 import P3
+from .production_4 import P4
 from .production_5 import P5
 from .production_6 import P6
 from .production_9 import P9
@@ -14,6 +15,7 @@ __all__ = (
     P1,
     P2,
     P3,
+    P4,
     P5,
     P6,
     P9,
