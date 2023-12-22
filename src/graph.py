507cceddda271f6b73ae1ed7dbadf3ff1a1a8058
Kacper Kafara <kacperkafara@gmail.com>
2023-12-22 23:41:39 +0100

    Return node handle from Q/P edge adding methods (#36)
    * Return node handle from q/p edge adding methods
    
    * Fix incorrect comment

--- src/graph.py
+++ src/graph.py
@@ -19,6 +19,7 @@ def node_equality(nx_node_attrs_1, nx_node_attrs_2) -> bool:
         attrs_1.flag == attrs_2.flag
     )
 
+
 def edge_equality(nx_edge_attrs_1, nx_edge_attrs_2) -> bool:
     attrs_1: EdgeAttrs = nx_edge_attrs_1['payload']
     attrs_2: EdgeAttrs = nx_edge_attrs_2['payload']
@@ -47,14 +48,15 @@ class NodeHandleGenerator:
         return self._counter.__next__()
 
 
-
 class Graph:
     def __init__(self) -> None:
         self._graph = nx.Graph()
         self._node_handle_factory = NodeHandleGenerator(initial_value=0)
 
+
     def __contains__(self, node: NodeHandle) -> bool:
         return self._graph.has_node(node)
+
 
     def add_node(self, node: Node) -> NodeHandle:
         """ IMPORTANT:
@@ -68,8 +70,10 @@ class Graph:
         self._graph.add_node(node.handle, payload=node.attrs)
         return node.handle
 
+
     def add_node_collection(self, node_collection: Iterable[Node]) -> Iterable[NodeHandle]:
         return [self.add_node(node) for node in node_collection]
+
 
     def remove_node(self, handle: NodeHandle):
         self._graph.remove_node(handle)
@@ -79,27 +83,34 @@ class Graph:
         for node in node_collection:
             self.remove_node(node)
 
+
     def add_edge(self, edge: Edge):
         self._graph.add_edge(u_of_edge=edge.u, v_of_edge=edge.v, payload=edge.attrs)
+
 
     def add_edge_collection(self, edge_collection: Iterable[Edge]):
         for edge in edge_collection:
             self.add_edge(edge)
 
+
     def remove_edge(self, handle_1: NodeHandle, handle_2: NodeHandle):
         self._graph.remove_edge(handle_1, handle_2)
+
 
     def remove_edge_with_endpoints(self, edge: EdgeEndpoints):
         # Leaving method `remove_edge` to avoid refactorings & keeping backward compat
         self._graph.remove_edge(edge[0], edge[1])
 
+
     def remove_edge_collection(self, edge_collection: Iterable[Edge]):
         for edge in edge_collection:
             self.remove_edge(edge.u, edge.v)
 
+
     def generate_subgraphs_isomorphic_with(self, other: 'Graph') -> Iterable[GraphMapping]:
         gm = nx.isomorphism.GraphMatcher(self._graph, other.nx_graph, node_match=node_equality, edge_match=edge_equality)
         return gm.subgraph_isomorphisms_iter()
+
 
     def generate_subgraphs_monomorphic_with(self, other: 'Graph') -> Iterable[GraphMapping]:
         gm = nx.isomorphism.GraphMatcher(self._graph, other.nx_graph, node_match=node_equality, edge_match=edge_equality)
@@ -109,6 +120,7 @@ class Graph:
     def node_for_handle(self, handle: NodeHandle) -> Node:
         return Node(handle=handle, attrs=self[handle])
 
+
     def __getitem__(self, node: NodeHandle) -> NodeAttrs:
         # Actually we want to throw in case there is no such node
         try:
@@ -117,11 +129,14 @@ class Graph:
             print(f"Attempt to get node with handle {node} that does not exist in graph")
             raise err
 
+
     def node_attrs(self, node: NodeHandle) -> NodeAttrs:
         return self[node]
 
+
     def edge_attrs(self, edge: EdgeEndpoints) -> EdgeAttrs:
         return self._graph[edge[0]][edge[1]]['payload']
+
 
     def edge_for_handles(self, handle_1: NodeHandle, handle_2: NodeHandle) -> Edge:
         attrs = self.edge_attrs((handle_1, handle_2))
@@ -143,14 +158,23 @@ class Graph:
         nx.draw_networkx_edge_labels(self.nx_graph, pos=positions, edge_labels=edge_labels, **kwargs)
 
 
-    def add_q_hyperedge(self, nodes: tuple[Node, Node, Node, Node], edge_attrs: EdgeAttrs, q_node_handle: NodeHandle = None):
+    def add_q_hyperedge(self, nodes: tuple[Node, Node, Node, Node], edge_attrs: EdgeAttrs, q_node_handle: NodeHandle = None) -> NodeHandle:
+        """ Add Q-hyperedge to the graph.
+
+        :param nodes: tuple/list of FOUR nodes, that the Q-hyperedge should connect
+        :param edge_attrs: shared attributes for all the edges that the hyperedge is comprised of
+        :param q_node_handle: optional node handle for the P-hypernode; if not specified an graph-unique id will be assigned automatically
+        :return: handle of newly created node representing Q-edge centre; might be useful if you want to remove this hyperedge
+        """
         assert len(nodes) == 4
         assert edge_attrs.kind == 'q'
         x, y = util.avg_point_from_nodes(nodes)
         node_attrs = NodeAttrs('q', x, y, edge_attrs.flag)
         q_node = Node(node_attrs, q_node_handle)
-        self.add_node(q_node)
+        q_node_handle = self.add_node(q_node)
         self.add_edge_collection(Edge(node.handle, q_node.handle, edge_attrs) for node in nodes)
+        return q_node_handle
+
 
     def remove_q_hyperedge(self, q_node_handle: NodeHandle):
         """ Remove Q hyperedge with given (hyper)node in the centre.
@@ -160,22 +184,24 @@ class Graph:
         assert q_node_attrs.label == 'q', f"Attempt to remove q hyperedge with handle for node with attrs {q_node_attrs}, label={q_node_attrs.label}"
         self.remove_node(q_node_handle)
 
-    def add_p_hyperedge(self, nodes: tuple[Node, Node, Node, Node, Node], edge_attrs: EdgeAttrs, p_node_handle: NodeHandle = None, p_node_coords: tuple[float, float] = None):
+
+    def add_p_hyperedge(self, nodes: tuple[Node, Node, Node, Node, Node], edge_attrs: EdgeAttrs, p_node_handle: NodeHandle = None, p_node_coords: tuple[float, float] = None) -> NodeHandle:
         """ Add P-hyperedge to the graph.
 
         :param nodes: tuple/list of FIVE nodes, that the P-hyperedge should connect
         :param edge_attrs: shared attributes for all the edges that the hyperedge is comprised of
         :param p_node_handle: optional node handle for the P-hypernode; if not specified an graph-unique id will be assigned automatically
         :param p_node_coords: optional tuple with coordinates for the P-hypernode; if not specified the position will be calculated as mean point between: nodes[0], nodes[1], nodes[2], nodes[3], nodes[4]
+        :return: handle of newly created node representing P-edge centre; might be useful if you want to remove this hyperedge
         """
-
         assert len(nodes) == 5
         assert edge_attrs.kind == 'p'
         x, y = p_node_coords if p_node_coords is not None else util.avg_point_from_nodes(nodes)
         node_attrs = NodeAttrs('p', x, y, edge_attrs.flag)
         p_node = Node(node_attrs, handle=p_node_handle)
-        self.add_node(p_node)
+        p_node_handle = self.add_node(p_node)
         self.add_edge_collection(Edge(node.handle, p_node.handle, edge_attrs) for node in nodes)
+        return p_node_handle
 
 
     def remove_p_hyperedge(self, p_node_handle: NodeHandle):
@@ -212,12 +238,15 @@ class Graph:
 
         return h_node
 
+
     def dump_edge(self, fname):
         nx.write_edgelist(self._graph, fname)
+
 
     @property
     def nx_graph(self) -> nx.Graph:
         return self._graph
+
 
     @property
     def node_handle_factory(self) -> Callable[[], int]:
