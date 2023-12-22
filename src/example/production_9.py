b6acc72c0951957122edd0c2b42e4ac4fbea2473
Kacper Kafara <kacperkafara@gmail.com>
2023-12-22 15:27:37 +0100

    Migrate all examples

--- src/example/production_9.py
+++ src/example/production_9.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 from production.production_9 import P9
 from production.production_16 import P16
 
-def test_16_9():
 
+def main():
     graph = basic_graph.basic_pentagon()
     graph.display()
     plt.show()
     P16()(graph)
     P9()(graph)
+
+
+if __name__ == "__main__":
+    main()
 
