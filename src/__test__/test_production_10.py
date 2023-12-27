e3f244d5acd0a215d64a466267050ac9eecc9f00
Akatroj <yjkjgdjfhjrdhgjh@gmail.com>
2023-12-27 21:06:35 +0100

    fix tests for production 9&10, add tests for prod 16

--- src/__test__/test_production_10.py
+++ src/__test__/test_production_10.py
@@ -1,40 +1,27 @@
 import unittest
-import itertools as it
 
 from matplotlib import pyplot as plt
 
 from graph import Graph
-from model import Node, NodeAttrs, EdgeAttrs, Edge
+from model import EdgeEndpoints, Node, NodeAttrs, EdgeAttrs, Edge
 from production import P10
-from basic_graph import basic_pentagon
 
 class TestProduction10(unittest.TestCase):
-    def is_any_mapping_feasible(self, graph: Graph):
-        p10 = P10()
-        mapping_gen = graph.generate_subgraphs_monomorphic_with(p10.get_lhs())
-        return any(p10.is_mapping_feasible(graph, mapping) for mapping in mapping_gen)
+    def validate_shape_after_production(self, graph: Graph, previous_edges: list[Edge]):
+        new_nodes = []
+        for edge in previous_edges:
+            node_1, node_2, flag = edge.u, edge.v, edge.attrs.flag
+            v = graph.get_node_between((node_1, node_2))
+            self.assertEqual(v.attrs.flag, not flag)
+            new_nodes.append(v)
+        
+        center_node = graph.get_real_nodes()[-1]
+        self.assertFalse(center_node.attrs.flag) # center node not hanging
+        # center node connected to new nodes
+        for node in new_nodes:
+            self.assertTrue(graph.has_edge(EdgeEndpoints(node.handle, center_node.handle)))
 
-    def test_production_can_be_applied_on_lhs_graph(self):
-        graph = Graph()
-
-        node_0 = Node(NodeAttrs('v', 0, 0, False))
-        node_1 = Node(NodeAttrs('v', 1, 0, False))
-        node_2 = Node(NodeAttrs('v', 1, 1, False))
-        node_3 = Node(NodeAttrs('v', 0, 1, False))
-        
-        node_4 = Node(NodeAttrs('v', 1.5, 0.5, False))
-        nodes = [node_0, node_1, node_2, node_3, node_4]
-
-        graph.add_node_collection(nodes)
-        graph.add_p_hyperedge((node_0, node_1, node_2, node_3, node_4), EdgeAttrs('p', True), p_node_coords=(0.5, 0.5))
-        
-        edges = [(node_0, node_1), (node_1, node_4), (node_4, node_2), (node_2, node_3), (node_3, node_0)]
-        for node_1, node_2 in edges:
-            graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
-        
-        graph.split_edge_with_vnode(edge=(0, 1), node_flag=True)
-        self.assertTrue(self.is_any_mapping_feasible(graph))
-        
+      
     def test_production_can_be_applied_on_more_complex_graph(self):
         graph = Graph()
 
@@ -52,45 +39,26 @@ class TestProduction10(unittest.TestCase):
         graph.add_node_collection(nodes)
         
         graph.add_p_hyperedge((node_0, node_1, node_2, node_3, node_4), EdgeAttrs('p', True), p_node_coords=(0.5, 0.5))
-        graph.add_p_hyperedge((node_5, node_6, node_2, node_3, node_7), EdgeAttrs('p', True), p_node_coords=(1.5, 0.5))
 
-
-        edges = [(node_0, node_1), (node_1, node_4), (node_4, node_2), (node_3, node_0), 
-                 (node_5, node_6), (node_6, node_7), (node_7, node_2), (node_2, node_5)]
-        
-        graph.add_edge(Edge(node_2.handle, node_3.handle, EdgeAttrs(kind='e', flag=False)))
-
-        for node_1, node_2 in edges:
+        border_edges = [(node_0, node_1), (node_1, node_5), (node_5, node_6), (node_6, node_7), 
+                 (node_7, node_2), (node_2, node_4), (node_4, node_3), (node_3, node_0)]
+        shared_edges = [(node_1, node_2)]
+    
+        for node_1, node_2 in border_edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
 
-        graph.split_edge_with_vnode(edge=(0, 1), node_flag=True)
-        self.assertTrue(self.is_any_mapping_feasible(graph))
-        
-    def test_node_no_longer_hanging_after_production_applied(self):
-        graph = Graph()
+        for node_1, node_2 in shared_edges:
+            graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=False)))
 
-        node_0 = Node(NodeAttrs('v', 0, 0, False))
-        node_1 = Node(NodeAttrs('v', 1, 0, False))
-        node_2 = Node(NodeAttrs('v', 1, 1, False))
-        node_3 = Node(NodeAttrs('v', 0, 1, False))
+        v_node = graph.split_edge_with_vnode(edge=(node_1.handle, node_2.handle), node_flag=True)
+        edges = graph.get_real_edges()
         
-        node_4 = Node(NodeAttrs('v', 1.5, 0.5, False))
-        nodes = [node_0, node_1, node_2, node_3, node_4]
-
-        graph.add_node_collection(nodes)
-        graph.add_p_hyperedge((node_0, node_1, node_2, node_3, node_4), EdgeAttrs('p', True), p_node_coords=(0.5, 0.5))
+        res = P10()(graph)
         
-        edges = [(node_0, node_1), (node_1, node_4), (node_4, node_2), (node_2, node_3), (node_3, node_0)]
-        for node_1, node_2 in edges:
-            graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
+        self.assertTrue(res)
+        self.assertFalse(graph.node_attrs(v_node.handle).flag)
+        self.validate_shape_after_production(graph, filter(lambda edge: edge.u<5 and edge.v<5, edges)) # only left pentagon
         
-        graph.split_edge_with_vnode(edge=(0, 1), node_flag=True)
-        
-        
-        P10()(graph)
-
-        self.assertFalse(graph.node_attrs(6).flag)
-
     def test_production_cannot_be_applied_if_no_hanging(self):
         graph = Graph()
 
@@ -109,8 +77,9 @@ class TestProduction10(unittest.TestCase):
         for node_1, node_2 in edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
         
-        graph.split_edge_with_vnode(edge=(0, 1), node_flag=False)
-        self.assertFalse(self.is_any_mapping_feasible(graph))
+        graph.split_edge_with_vnode(edge=(node_1.handle, node_2.handle), node_flag=False)
+
+        self.assertFalse(P10()(graph))
         
     def test_production_cannot_be_applied_if_R_false(self):
         graph = Graph()
@@ -130,8 +99,8 @@ class TestProduction10(unittest.TestCase):
         for node_1, node_2 in edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
         
-        graph.split_edge_with_vnode(edge=(0, 1), node_flag=True)
-        self.assertFalse(self.is_any_mapping_feasible(graph))
+        graph.split_edge_with_vnode(edge=(node_1.handle, node_2.handle), node_flag=True)
+        self.assertFalse(P10()(graph))
 
 
 if __name__ == '__main__':
--- src/__test__/test_production_9.py
+++ src/__test__/test_production_9.py
@@ -1,17 +1,27 @@
 import unittest
-import itertools as it
+
+from matplotlib import pyplot as plt
 
 from graph import Graph
-from model import Node, NodeAttrs, EdgeAttrs, Edge
+from model import EdgeEndpoints, Node, NodeAttrs, EdgeAttrs, Edge
 from production import P9
-from basic_graph import basic_pentagon
 
 class TestProduction9(unittest.TestCase):
-    def is_any_mapping_feasible(self, graph: Graph):
-        p9 = P9()
-        mapping_gen = graph.generate_subgraphs_monomorphic_with(p9.get_lhs())
-        return any(p9.is_mapping_feasible(graph, mapping) for mapping in mapping_gen)
+    def validate_shape_after_production(self, graph: Graph, previous_edges: list[Edge]):
+        new_nodes = []
+        for edge in previous_edges:
+            node_1, node_2, flag = edge.u, edge.v, edge.attrs.flag
+            v = graph.get_node_between((node_1, node_2))
+            self.assertEqual(v.attrs.flag, not flag)
+            new_nodes.append(v)
+        
+        center_node = graph.get_real_nodes()[-1]
+        self.assertFalse(center_node.attrs.flag) # center node not hanging
+        # center node connected to new nodes
+        for node in new_nodes:
+            self.assertTrue(graph.has_edge(EdgeEndpoints(node.handle, center_node.handle)))
 
+    
     def test_production_can_be_applied_on_lhs_graph(self):
         graph = Graph()
 
@@ -30,9 +40,51 @@ class TestProduction9(unittest.TestCase):
         for node_1, node_2 in edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
         
-        self.assertTrue(self.is_any_mapping_feasible(graph))
+        edges = graph.get_real_edges()
+        
+        res = P9()(graph)
+        
+        self.assertTrue(res)
+        self.validate_shape_after_production(graph, edges)
         
         
+    def test_production_can_be_applied_on_more_complex_graph(self):
+        graph = Graph()
+
+        node_0 = Node(NodeAttrs('v', 0, 0, False))
+        node_1 = Node(NodeAttrs('v', 1, 0, False))
+        node_2 = Node(NodeAttrs('v', 1, 1, False))
+        node_3 = Node(NodeAttrs('v', 0, 1, False))
+        node_4 = Node(NodeAttrs('v', 0.5, 1.5, False))
+        
+        node_5 = Node(NodeAttrs('v', 2, 0, False)) 
+        node_6 = Node(NodeAttrs('v', 2, 1, False))
+        node_7 = Node(NodeAttrs('v', 1.5, 1.5, False))
+
+        nodes = [node_0, node_1, node_2, node_3, node_4, node_5, node_6, node_7] 
+        graph.add_node_collection(nodes)
+        
+        graph.add_p_hyperedge((node_0, node_1, node_2, node_3, node_4), EdgeAttrs('p', True), p_node_coords=(0.5, 0.5))
+
+
+        border_edges = [(node_0, node_1), (node_1, node_5), (node_5, node_6), (node_6, node_7), 
+                 (node_7, node_2), (node_2, node_4), (node_4, node_3), (node_3, node_0)]
+        
+        shared_edges = [(node_1, node_2)]
+    
+        for node_1, node_2 in border_edges:
+            graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
+
+        for node_1, node_2 in shared_edges:
+            graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=False)))
+
+        edges = graph.get_real_edges()
+        
+        res = P9()(graph)
+        
+        self.assertTrue(res)
+        self.validate_shape_after_production(graph, filter(lambda edge: edge.u<5 and edge.v<5, edges)) # only left pentagon
+
     def test_production_cannot_be_applied_if_hanging(self):
         graph = Graph()
 
@@ -51,7 +103,7 @@ class TestProduction9(unittest.TestCase):
         for node_1, node_2 in edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
         
-        self.assertFalse(self.is_any_mapping_feasible(graph))
+        self.assertFalse(P9()(graph))
         
     def test_production_cannot_be_applied_if_R_false(self):
         graph = Graph()
@@ -71,7 +123,7 @@ class TestProduction9(unittest.TestCase):
         for node_1, node_2 in edges:
             graph.add_edge(Edge(node_1.handle, node_2.handle, EdgeAttrs(kind='e', flag=True)))
         
-        self.assertFalse(self.is_any_mapping_feasible(graph))
+        self.assertFalse(P9()(graph))
 
 
 
