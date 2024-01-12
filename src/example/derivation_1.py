fde298f675f24590e993ccb7b37fac3017ae5d5a
Kacper Kafara <kacperkafara@gmail.com>
2024-01-12 16:49:35 +0100

    Better plots & driver delegate (#44)
    * Various improvements
    
    * Do not uploag pngs

--- src/example/derivation_1.py
+++ src/example/derivation_1.py
@@ -4,7 +4,8 @@ from production import (
     P9, P8, P16, P7, P2, P1, P3
 )
 from model import Node, NodeAttrs, Edge, EdgeAttrs
-from driver import Driver, FixedInput
+from driver import Driver, FixedInput, DrawingDriverDelegate
+from pathlib import Path
 import itertools as it
 import matplotlib.pyplot as plt
 import basic_graph as gu
@@ -14,11 +15,11 @@ def create_graph() -> Graph:
     graph = Graph()
 
     inner_nodes = [
-        Node(NodeAttrs('v', 0, 0, False)),
-        Node(NodeAttrs('v', 1, 0, False)),
-        Node(NodeAttrs('v', 1.5, 0.5, False)),
-        Node(NodeAttrs('v', 1, 1, False)),
-        Node(NodeAttrs('v', 0, 1, False)),
+        Node(NodeAttrs('v', -0.5, -0.5, False)),
+        Node(NodeAttrs('v', 0.5, -0.5, False)),
+        Node(NodeAttrs('v', 0.8, 0.5, False)),
+        Node(NodeAttrs('v', 0.5, 1.5, False)),
+        Node(NodeAttrs('v', -0.5, 1.5, False)),
     ]
 
     outer_nodes = [
@@ -50,9 +51,25 @@ def create_graph() -> Graph:
     return graph
 
 
+class AxesProvider:
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        _fig, axes = plt.subplots(nrows=1, ncols=1)
+        return axes
+
 def main():
+    savedir = Path('docs/derivation_1/')
+    if not savedir.is_dir():
+        savedir.mkdir(parents=True)
+
     graph = create_graph()
+    graph.display()
+    plt.show()
+
     driver = Driver()
+    driver.delegate = DrawingDriverDelegate(savedir)
 
     driver.execute_production_sequence(graph, [
         P16(),
@@ -70,8 +87,3 @@ def main():
         P3(),
         P1()
     ])
-
-    graph.display()
-
-
-    plt.show()
